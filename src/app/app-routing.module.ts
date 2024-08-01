fd1945c09489964e1423b980cf1ffc9a4e42980d
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2024-07-31 22:16:44 -0300

    commit dentro do vs code

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from '../components/home/home.component';
+import { ExperienciasComponent } from '../components/experiencias/experiencias.component';
+import { SkillsComponent } from '../components/skills/skills.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'main',
+    component: HomeComponent,
+  },
+  {
+    path:'experiencias',
+    component: ExperienciasComponent,
+  },
+  {
+    path:'skills',
+    component: SkillsComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
