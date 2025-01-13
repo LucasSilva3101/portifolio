f00e3f9f5e7eaadda9ff1d274145720603bb303e
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2025-01-13 20:12:44 -0300

    Add card da aba Experiencias + logo

--- src/components/experiencias/experiencias.component.ts
+++ src/components/experiencias/experiencias.component.ts
@@ -1,19 +1,16 @@
 import { Component } from '@angular/core';
+import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-experiencias',
   templateUrl: './experiencias.component.html',
   styleUrls: ['./experiencias.component.css']
 })
+
 export class ExperienciasComponent {
 
   constructor() { }
 
   ngOnInit() {
-  }
-  isExpanded = false;
-
-  toggleCard() {
-    this.isExpanded = !this.isExpanded;
   }
 }
