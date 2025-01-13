01bcb8a41dcb0631932c050369f2441e40907822
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2025-01-13 20:21:57 -0300

    Add array de experiencias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ExperienciasComponent } from '../components/experiencias/experiencias.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ExperienciasComponent
   ],
   imports: [
     BrowserModule,
--- src/components/experiencias/experiencias.component.ts
+++ src/components/experiencias/experiencias.component.ts
@@ -13,4 +13,28 @@ export class ExperienciasComponent {
 
   ngOnInit() {
   }
+
+  experiences = [
+    {
+      id: 1,
+      title: 'SulAmérica',
+      logo: 'assets/logo-sulamerica.jpg',
+      description: 'Trabalhei como estagiário de TI...',
+      details: ['Desenvolvimento em Angular', 'Manutenção de sistemas legados', 'Automação de relatórios'],
+    },
+    {
+      id: 2,
+      title: 'Empresa 2',
+      logo: 'assets/logo-empresa2.jpg',
+      description: 'Atuei em projetos web...',
+      details: ['Integração com APIs', 'Desenvolvimento front-end', 'Otimização de performance'],
+    },
+    {
+      id: 3,
+      title: 'Empresa 3',
+      logo: 'assets/logo-empresa3.jpg',
+      description: 'Responsável pelo desenvolvimento...',
+      details: ['Criação de módulos em Angular', 'Testes unitários', 'Documentação técnica'],
+    },
+  ];
 }
