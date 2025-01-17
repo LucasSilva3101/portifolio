15f65253acc9cdcfc8edf51f6d15325b5a5315f2
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2025-01-16 21:46:56 -0300

    ajuste de dropdown-menu retirando comentarios add function para copiar email para area de trabalho

--- src/components/nav-bar/nav-bar.component.ts
+++ src/components/nav-bar/nav-bar.component.ts
@@ -5,11 +5,16 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './nav-bar.component.html',
   styleUrls: ['./nav-bar.component.css']
 })
-export class NavBarComponent implements OnInit {
+export class NavBarComponent {
 
-  constructor() { }
-
-  ngOnInit() {
+  LinkedinLink:string = "https://www.linkedin.com/in/lucas-silva-b89171269/";
+  
+  copyToClipboard(text: string): void {
+    navigator.clipboard.writeText(text).then(() => {
+      alert('E-mail copiado para a área de transferência!');
+    }).catch((err) => {
+      console.error('Falha ao copiar o texto: ', err);
+      alert('Não foi possível copiar o e-mail.');
+    });
   }
-
 }
