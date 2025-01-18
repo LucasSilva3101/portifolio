9af2c94651bb3f15009972093388a62d73a7ffc7
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2025-01-17 22:20:19 -0300

    atualizando package tentativa 11

--- src/components/experiencias/experiencias.component.ts
+++ src/components/experiencias/experiencias.component.ts
@@ -2,9 +2,10 @@ import { Component } from '@angular/core';
 import { trigger, state, style, transition, animate } from '@angular/animations';
 
 @Component({
-  selector: 'app-experiencias',
-  templateUrl: './experiencias.component.html',
-  styleUrls: ['./experiencias.component.css']
+    selector: 'app-experiencias',
+    templateUrl: './experiencias.component.html',
+    styleUrls: ['./experiencias.component.css'],
+    standalone: false
 })
 
 export class ExperienciasComponent {
