8f8ca53f78e1d6a6873fc4e5908f9b7890b4f563
DESKTOP-KTGIN98\Lucas <lucasdsg31@outlook.com>
2025-02-04 14:15:44 -0300

    add scroll mais suave
    animação descripition-container
    
    animação about-container

--- src/components/home/home.component.ts
+++ src/components/home/home.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
-    selector: 'app-home',
-    templateUrl: './home.component.html',
-    styleUrls: ['./home.component.css']
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
 
   linkParaCurriculo = 'assets/curriculo.pdf';
+  isVisible = false;
 
-  constructor() { }
+  constructor(private el: ElementRef) {}
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    window.addEventListener('scroll', this.checkScroll);
   }
+
+  checkScroll = () => {
+    const section = this.el.nativeElement.querySelector('.about-container');
+    if (section) {
+      const rect = section.getBoundingClientRect();
+      const windowHeight = window.innerHeight;
+
+      if (rect.top < windowHeight * 0.75) {
+        this.isVisible = true;
+        window.removeEventListener('scroll', this.checkScroll);
+      }
+    }
+  };
 }
